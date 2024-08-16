f01a34fa77ad30cc562fea5203046605e060f608
dejosemario <josemariaodumodu@gmail.com>
2024-08-16 10:13:37 +0100

    fix: fix config issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // This allows Vite to listen on all network interfaces
-    port: 5173,     // Ensure this is the port Vite is using
+    host: "0.0.0.0", // This allows Vite to listen on all network interfaces
+    port: 5173, // Ensure this is the port Vite is using
     proxy: {
-      '/api': {
-        target: 'https://server-z0w0.onrender.com/api/v1',
+      "/api": {
+        target: (import.meta as any).env.VITE_BACKEND_URL, // Ensure you define this in your .env file
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
-
-
-// vite.con
+});
